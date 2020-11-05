f527c33eb3d3009178d32dffe6760c691383e80a
huyfr <61008601+huyfr@users.noreply.github.com>
2020-11-05 10:27:32 +0700

    add constants for organization credentials

--- dao/src/main/java/org/thingsboard/server/dao/model/ModelConstants.java
+++ dao/src/main/java/org/thingsboard/server/dao/model/ModelConstants.java
@@ -42,6 +42,7 @@ public class ModelConstants {
     public static final String TENANT_ID_PROPERTY = "tenant_id";
     public static final String CUSTOMER_ID_PROPERTY = "customer_id";
     public static final String DEVICE_ID_PROPERTY = "device_id";
+    public static final String ORGANIZATION_ID_PROPERTY = "organization_id";
     public static final String TITLE_PROPERTY = "title";
     public static final String ALIAS_PROPERTY = "alias";
     public static final String SEARCH_TEXT_PROPERTY = "search_text";
@@ -277,6 +278,16 @@ public class ModelConstants {
 
     public static final String DEVICE_CREDENTIALS_BY_DEVICE_COLUMN_FAMILY_NAME = "device_credentials_by_device";
     public static final String DEVICE_CREDENTIALS_BY_CREDENTIALS_ID_COLUMN_FAMILY_NAME = "device_credentials_by_credentials_id";
+
+
+    /**
+     * Cassandra organization_credentials constants.
+     */
+    public static final String ORGANIZATION_CREDENTIALS_COLUMN_FAMILY_NAME = "organization_credentials";
+    public static final String ORGANIZATION_CREDENTIALS_DEVICE_ID_PROPERTY = ORGANIZATION_ID_PROPERTY;
+    public static final String ORGANIZATION_CREDENTIALS_CREDENTIALS_TYPE_PROPERTY = "credentials_type";
+    public static final String ORGANIZATION_CREDENTIALS_CREDENTIALS_ID_PROPERTY = "credentials_id";
+    public static final String ORGANIZATION_CREDENTIALS_CREDENTIALS_VALUE_PROPERTY = "credentials_value";
 
     /**
      * Cassandra widgets_bundle constants.
